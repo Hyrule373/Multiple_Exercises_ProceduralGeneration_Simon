fdb80f4f59fb98cc3ee69a3bded63a71b4f1f1fc
Simon_Aga <147041297+Hyrule373@users.noreply.github.com>
2025-11-03 15:30:14 +0100

    [ADD]

---
+++ Assets/Components/ProceduralGeneration/0_SimpleRoomPlacement/SimpleRoomPlacement.cs
@@ -0,0 +1,73 @@
+﻿using System.Threading;
+using Cysharp.Threading.Tasks;
+using UnityEngine;
+using VTools.Grid;
+using VTools.ScriptableObjectDatabase;
+
+namespace Components.ProceduralGeneration.SimpleRoomPlacement
+{
+    [CreateAssetMenu(menuName = "Procedural Generation Method/Simple Room Placement")]
+    public class SimpleRoomPlacement : ProceduralGenerationMethod
+    {
+        [Header("Room Parameters")]
+        [SerializeField] private int _maxRooms = 10;
+        
+        protected override async UniTask ApplyGeneration(CancellationToken cancellationToken)
+        {            
+            for (int i = 0; i < _maxSteps; i++)
+            {
+                int x = RandomService.Range(0, _maxSteps);
+                int y = RandomService.Range(0, _maxSteps);
+                int rWidth = RandomService.Range(0, 10);
+                int rHeight = RandomService.Range(0, 10);
+
+                RectInt room = new RectInt(x, y, rWidth, rHeight);
+
+                this.PlaceRoom(room);
+
+                // Waiting between steps to see the result.
+                await UniTask.Delay(GridGenerator.StepDelay, cancellationToken : cancellationToken);
+            }
+            
+
+
+            // Final ground building.
+            BuildGround();
+        }
+        
+        private void BuildGround()
+        {
+            var groundTemplate = ScriptableObjectDatabase.GetScriptableObject<GridObjectTemplate>("Grass");
+            
+            // Instantiate ground blocks
+            for (int x = 0; x < Grid.Width; x++)
+            {
+                for (int z = 0; z < Grid.Lenght; z++)
+                {
+                    if (!Grid.TryGetCellByCoordinates(x, z, out var chosenCell))
+                    {
+                        Debug.LogError($"Unable to get cell on coordinates : ({x}, {z})");
+                        continue;
+                    }
+                    
+                    GridGenerator.AddGridObjectToCell(chosenCell, groundTemplate, false);
+                }
+            }
+        }
+
+        private void PlaceRoom(RectInt room)
+        {
+            for (int ix = room.xMin; ix < room.xMax; ix++)
+            {
+                for (int iy = room.yMin; iy < room.yMax; iy++)
+                {
+                    if (Grid.TryGetCellByCoordinates(ix, iy, out var cell))
+                    {
+                        AddTileToCell(cell, ROOM_TILE_NAME, true);
+                    }                        
+                }
+            }
+        }
+
+    }
+}
