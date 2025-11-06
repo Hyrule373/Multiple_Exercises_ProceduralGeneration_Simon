e808cfd938cb659524b1ded901badd1ab4e166bd
Simon_Aga <147041297+Hyrule373@users.noreply.github.com>
2025-11-06 15:17:17 +0100

    [ADD]

--- Assets/Components/ProceduralGeneration/4 - FastNoiseLite/FastNoiseMap.cs
+++ Assets/Components/ProceduralGeneration/4 - FastNoiseLite/FastNoiseMap.cs
@@ -1,7 +1,9 @@
 ﻿using Cysharp.Threading.Tasks;
 using NUnit.Framework;
+using System;
 using System.Collections.Generic;
 using System.Threading;
+using Unity.Mathematics;
 using Unity.VisualScripting;
 using UnityEngine;
 using VTools.Grid;
@@ -12,57 +14,88 @@ namespace Components.ProceduralGeneration.SimpleRoomPlacement
     [CreateAssetMenu(menuName = "Procedural Generation Method/Fast Noise Map")]
     public class FastNoiseMap : ProceduralGenerationMethod
     {
-        [Header("Room Parameters")]
-        [SerializeField] private int _maxRooms = 10;
+        [Header("Global Data")]
+        [SerializeField] private FastNoiseLite.NoiseType noiseType;
+        [SerializeField, UnityEngine.Range(0.001f, 1f)] private float frequency = 0.001f;
+        [SerializeField, UnityEngine.Range(0.001f, 1f)] private float amplitude = 0.001f;
+
+        [Header("Fractal Data")]
+        [SerializeField] private FastNoiseLite.FractalType fractalType = FastNoiseLite.FractalType.None;
+        [SerializeField] private int Octaves;
+        [SerializeField] private float Lacunarity = 0.001f;
+
+        [Header("Tile Depth")]
+        [SerializeField, UnityEngine.Range( -1f, 1f)] private float WaterHeight = -0.33f;
+        [SerializeField, UnityEngine.Range(-1f, 1f)] private float SandHeight = 0.33f;
+        [SerializeField, UnityEngine.Range(-1f, 1f)] private float GrassHeight = 1f;
 
         protected override async UniTask ApplyGeneration(CancellationToken cancellationToken)
         {
-            
-            for (int i = 0; i < _maxSteps; i++)
+            FastNoiseLite noise = new FastNoiseLite(RandomService.Seed);
+
+            InitializeNoise(noise);
+
+            // Height Map
+            float[,] noiseData = new float[Grid.Width, Grid.Lenght];
+
+            for (int x = 0; x < Grid.Lenght; x++)
             {
-
-                await UniTask.Delay(GridGenerator.StepDelay, cancellationToken: cancellationToken);
-            }
-
-            // Final ground building.
-            BuildGround();
-        }
-
-        private void BuildGround()
-        {
-            var groundTemplate = ScriptableObjectDatabase.GetScriptableObject<GridObjectTemplate>("Grass");
-
-            // Instantiate ground blocks
-            for (int x = 0; x < Grid.Width; x++)
-            {
-                for (int z = 0; z < Grid.Lenght; z++)
+                for (int y = 0; y < Grid.Width; y++)
                 {
-                    if (!Grid.TryGetCellByCoordinates(x, z, out var chosenCell))
-                    {
-                        //Debug.LogError($"Unable to get cell on coordinates : ({x}, {z})");
-                        continue;
-                    }
+                    cancellationToken.ThrowIfCancellationRequested();
 
-                    GridGenerator.AddGridObjectToCell(chosenCell, groundTemplate, false);
-                }
-            }
-        }
+                    noiseData[x, y] = noise.GetNoise(x, y);
 
-        private void PlaceRoom(RectInt room)
-        {
-            for (int ix = room.xMin; ix < room.xMax; ix++)
-            {
-                for (int iy = room.yMin; iy < room.yMax; iy++)
-                {
-                    if (Grid.TryGetCellByCoordinates(ix, iy, out var cell))
+                    if (Grid.TryGetCellByCoordinates(x, y, out Cell cell))
                     {
-                        AddTileToCell(cell, ROOM_TILE_NAME, true);
+                        float height = noise.GetNoise(x, y);
+
+                        string tileName;
+
+                        if (height <= WaterHeight)
+                        {
+                            tileName = WATER_TILE_NAME;
+                        }
+                        else if (height > WaterHeight && height <= SandHeight)
+                        {
+                            tileName = SAND_TILE_NAME;
+                        }
+                        else
+                        {
+                            tileName = GRASS_TILE_NAME;
+                        }
+
+                        AddTileToCell(cell, tileName, true);
                     }
                 }
             }
         }
 
+        void InitializeNoise(FastNoiseLite noise)
+        {
+            //Setting Global Data
+            noise.SetNoiseType(noiseType);
+            noise.SetFrequency(frequency);
 
+            //Setting Fractal
+            noise.SetFractalType(fractalType);
+            noise.SetFractalOctaves(Octaves);
+            noise.SetFractalLacunarity(Lacunarity);
+        }
 
+        void CreateTile(int x, int y, string type)
+        {
+            if (Grid.TryGetCellByCoordinates(x, y, out Cell cell))
+            {
+                AddTileToCell(cell, type, true);
+            }
+        }
+
+        float GetNoiseData(int x, int y, FastNoiseLite noise)
+        {
+            var amp = noise.GetNoise(x, y) * amplitude;
+
+            return Math.Clamp(amp, -1f, 1f);
+        }
     }
 }
